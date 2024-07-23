3429ee553198852b33be0aafc627b7d9dbed2785
Rhulani Mogotsi <rhulanimogotsi@gmail.com>
2024-07-23 21:25:16 +0200

    modified to take user input

--- helloWorld.py
+++ helloWorld.py
@@ -1,6 +1,7 @@
 """
-This is a program that will display "Git is awesome!"
-to the screen.
+This is a program that will take user input
+and print it to the screen.
 """
 
+user_input = input("Enter any phrase: ")
 print("Git is awesome!")
